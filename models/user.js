ed7d6b3110bc2c9f0d2c54168aac3f403decd6aa
A7med-p <47med.aa3@gmail.com>
2025-08-01 12:20:39 +0300

    models/user.js is done

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  items: [{ type: mongoose.Schema.Types.ObjectId, ref:'Item'}]
+})
+
+userSchema.methods.toJSON = function() {
+  const user = this.toObject()
+  delete user.password
+  return user
+}
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 8)
+  }
+  next()
+})
+
+userSchema.methods.generateAuthToken = async function() {
+  const token = jwt.sign({ _id: this._id }, 'secret')
+  return token
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
